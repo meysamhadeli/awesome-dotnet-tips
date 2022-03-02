f6529f7ca3b354db73f089a2c6757c542b5b96e7
meysamhadeli <meysamhadeli@gmail.com>
2022-03-02 14:11:51 +0330

    add c# file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AwesomeDotNetTips
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("This is C# repository.");
+        }
+    }
+}
